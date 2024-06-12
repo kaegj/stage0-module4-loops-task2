7deb824576803281176f01cc771634f40372f2d0
kaegan <kkiefer4@student.umgc.edu>
2024-06-12 09:42:02 +0900

    Submission 1

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,5 +3,19 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
 
+        int sum = 1;
+        if (power > 0) {
+            int i = 1;
+            System.out.println(1);
+            while (i <= power) {
+                sum = sum * 2;
+                i++;
+                System.out.println(sum);
+            }
+        } else if (power == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println("too much power");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,24 @@
 package school.mjc.stage0.loops.task2;
 
+import java.sql.SQLOutput;
+
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i, number = 1, count;
+
+        while (number <= printToInclusive) {
+            count = 0;
+            i = 2;
+            while (i <= number / 2) {
+                if (number % i == 0) {
+                    count++;
+                    break;
+                }
+                i++;
+            }
+            if(count == 0 && number != 1) {
+                System.out.println(number);
+            }
+        }
     }
 }
